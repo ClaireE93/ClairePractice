bf163f3b8b29fc0f0e5b04ecb3559f9885544138
Claire Johnson <c.eliseee@gmail.com>
2017-06-14 14:29:20 -0700

    Added fade in/out with JS

--- gallery.js
+++ gallery.js
@@ -19,10 +19,11 @@ function populateGallery() {
 // let isImageClick = false;
 
 function imgPopup(ind) {
-    // document.getElementById('overlay').style.visibility = 'visible';
-    document.getElementById('overlay').style.display = 'flex';
-    // let popupCont = document.getElementById('img-popup-container');
+    // document.getElementById('overlay').style.display = 'flex';
+    fadeIn(document.getElementById('overlay'));
+    let popupCont = document.getElementById('img-popup-container');
     let popupImg = document.getElementById('img-popup-element');
+    popupImg.className = 'img-popup-element img-fadeIn';
     let popupTxt = document.getElementById('text-popup-element');
     if (popupImg.childNodes.length) {
       popupImg.removeChild(popupImg.childNodes[0]);
@@ -42,8 +43,7 @@ function imgPopup(ind) {
     popupImg.appendChild(img);
     let description = document.createTextNode(imgArr[ind].desc);
     popupTxt.appendChild(description);
-    // window.addEventListener('click', closePopup(), false);
-    // isImageClick = true;
+    fadeIn(document.getElementById('overlay'));
     isPopupOpen = true;
 }
 
@@ -58,10 +58,52 @@ document.addEventListener('click', function(event) {
     const isClickInside = specifiedElement.contains(eventTarget);
     if (!isClickInside && isPopupOpen) {
       isPopupOpen = false;
-      document.getElementById('overlay').style.display = 'none';
+      fadeOut(document.getElementById('overlay'));
     }
   }
 });
+
+// function fadeIn(el) {
+//   el.style.opacity = 0;
+
+//   var last = +new Date();
+//   var tick = function() {
+//     el.style.opacity = +el.style.opacity + (new Date() - last) / 600;
+//     last = +new Date();
+
+//     if (+el.style.opacity < 1) {
+//       (window.requestAnimationFrame && requestAnimationFrame(tick)) || setTimeout(tick, 16);
+//     }
+//   };
+
+//   tick();
+// }
+
+//Should I just use the function from map.js? Need to load that js file onto page? Which is more efficient?
+function fadeIn(el){
+  el.style.opacity = 0;
+  el.style.display = 'flex';
+
+  (function fade() {
+    var val = parseFloat(el.style.opacity);
+    if (!((val += 0.05) > 1)) {
+      el.style.opacity = val;
+      requestAnimationFrame(fade);
+    }
+  })();
+}
+
+function fadeOut(el){
+  el.style.opacity = 1;
+
+  (function fade() {
+    if ((el.style.opacity -= 0.05) < 0) {
+      el.style.display = "none";
+    } else {
+      requestAnimationFrame(fade);
+    }
+  })();
+}
 
 // TODO: move to external file
 let imgArr = [{src: "GalleryPhotos/20170115_103925.jpg", desc: "Donner Lake, Truckee, CA"}, {src: "GalleryPhotos/cottowood.jpg", desc: "White Sands National Park, NM" }, {src: "GalleryPhotos/DSC07725.jpg", desc: "White Sands National Park, NM"}, {src: "GalleryPhotos/DSC07899.jpg", desc: "Vancouver, Canada" }, {src: "GalleryPhotos/DSC07947.jpg", desc: "Vancouver, Canada"}, {src: "GalleryPhotos/DSC07949.jpg", desc: "Vancouver, Canada"}, {src: "GalleryPhotos/DSC08027.jpg", desc: "Squamish, Canada"}, {src: "GalleryPhotos/HalfDome.jpg", desc: "Yosemite, CA"}, {src: "GalleryPhotos/HiddenLake.jpg", desc: "Glacier National Park, MT"}, {src: "GalleryPhotos/Hieropolis.jpg", desc: "Hieropolis, Turkey"}, {src: "GalleryPhotos/HalfDome2.jpg", desc: "Yosemite, CA"}, {src: "GalleryPhotos/Waterfall.jpg", desc: "Crater Lake, OR"}];
--- map.js
+++ map.js
@@ -7,18 +7,34 @@ function changeImage(location) {
   return false;
 }
 
-function fadeIn(el) {
+
+//TRY TO MOVE TO CSS. Have "fadein" and "fadeout" class in CSS. Change CSS class to fade in/fade out
+
+// function fadeIn(el) {
+//   el.style.opacity = 0;
+
+//   var last = +new Date();
+//   var tick = function() {
+//     el.style.opacity = +el.style.opacity + (new Date() - last) / 900;
+//     last = +new Date();
+
+//     if (+el.style.opacity < 1) {
+//       (window.requestAnimationFrame && requestAnimationFrame(tick)) || setTimeout(tick, 16);
+//     }
+//   };
+
+//   tick();
+// }
+
+function fadeIn(el){
   el.style.opacity = 0;
+  el.style.display = 'flex';
 
-  var last = +new Date();
-  var tick = function() {
-    el.style.opacity = +el.style.opacity + (new Date() - last) / 900;
-    last = +new Date();
-
-    if (+el.style.opacity < 1) {
-      (window.requestAnimationFrame && requestAnimationFrame(tick)) || setTimeout(tick, 16);
+  (function fade() {
+    var val = parseFloat(el.style.opacity);
+    if (!((val += 0.05) > 1)) {
+      el.style.opacity = val;
+      requestAnimationFrame(fade);
     }
-  };
-
-  tick();
+  })();
 }
